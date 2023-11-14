51e43f519f2ddb5c9c55277613546300fee51a9f
@yla <@yla@DESKTOP-FOSSDE8>
2023-11-14 14:40:23 +0200

    added data

---
+++ assets/css/Home/data.js
@@ -0,0 +1,36 @@
+import IMG1 from "../../assets/products/product8.png";
+import IMG2 from "../../assets/products/product2.png";
+import IMG3 from "../../assets/products/product3.png";
+import IMG4 from "../../assets/products/product6.png";
+import IMG5 from "../../assets/products/product7.png";
+import IMG6 from "../../assets/products/product1.png";
+import IMG7 from "../../assets/products/product5.png";
+import IMG8 from "../../assets/products/product4.png";
+
+
+export const data = [
+  {
+    index: 1,
+    img: IMG1,
+    name: "IVAR SAE 0W20 API SN ",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 2,
+    img: IMG2,
+    name: "IVAR SAE 5W30 API SN",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 3,
+    img: IMG3,
+    name: "IVAR SAE 5W20 API SN",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+];
---
+++ assets/css/products/data.js
@@ -0,0 +1,76 @@
+import IMG1 from "../../assets/products/product8.png";
+import IMG2 from "../../assets/products/product2.png";
+import IMG3 from "../../assets/products/product3.png";
+import IMG4 from "../../assets/products/product6.png";
+import IMG5 from "../../assets/products/product7.png";
+import IMG6 from "../../assets/products/product1.png";
+import IMG7 from "../../assets/products/product5.png";
+import IMG8 from "../../assets/products/product4.png";
+
+
+export const data = [
+  {
+    index: 1,
+    img: IMG1,
+    name: "IVAR SAE 0W20 API SN ",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 2,
+    img: IMG2,
+    name: "IVAR SAE 5W30 API SN",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 3,
+    img: IMG3,
+    name: "IVAR SAE 5W20 API SN",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 4,
+    img: IMG4,
+    name: "IVAR SAE 10W30 API SN",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 5,
+    img: IMG5,
+    name: "IVAR SAE 10W40 API SN",
+    description:
+      "FULLY SYNTHETIC ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 6,
+    img: IMG6,
+    name: "IVAR SAE HD50 API CI-4",
+    description:
+      "FULLY SYNTHETIC DIESEL OIL",
+    price: "00.00$",
+  },
+  {
+    index: 7,
+    img: IMG7,
+    name: "IVAR SAE 20W50 API SL/CF",
+    description:
+      "FULLY SYNTHETIC DIESEL AND ENGINE OIL",
+    price: "00.00$",
+  },
+  {
+    index: 8,
+    img: IMG8,
+    name: "IVAR SAE 15W40 API SL/CF",
+    description:
+      "FULLY SYNTHETIC DIESEL AND ENGINE OIL",
+    price: "00.00$",
+  },
+];
